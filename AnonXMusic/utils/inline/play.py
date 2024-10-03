f969d284e14e625156c916bc39056ddcbf8a6185
TheCuteXTeam <herkoumail@gmail.com>
2024-10-03 21:29:11 +0530

    Update play.py

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -61,10 +61,10 @@ def stream_markup_timer(_, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
-                text="Uᴘᴅᴀᴛᴇs 📢", url=f"https://t.me/NeoUpdatess"
+                text="Uᴘᴅᴀᴛᴇs 📢", url=f"https://t.me/AryanUpdatess"
             ),
             InlineKeyboardButton(
-                text="Sᴜᴘᴘᴏʀᴛ 💬", url=f"https://t.me/Worldwide_Friends_Group0"
+                text="Sᴜᴘᴘᴏʀᴛ 💬", url=f"https://t.me/AryanMusicSupport"
           ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -76,10 +76,10 @@ def stream_markup(_, chat_id):
     buttons = [
         [
             InlineKeyboardButton(
-                text="Uᴘᴅᴀᴛᴇs 📢", url=f"https://t.me/NeoUpdatess"
+                text="Uᴘᴅᴀᴛᴇs 📢", url=f"https://t.me/AryanUpdatess"
             ),
             InlineKeyboardButton(
-                text="Sᴜᴘᴘᴏʀᴛ 💬", url=f"https://t.me/Worldwide_Friends_Group0"
+                text="Sᴜᴘᴘᴏʀᴛ 💬", url=f"https://t.me/AryanMusicSupport"
           ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
